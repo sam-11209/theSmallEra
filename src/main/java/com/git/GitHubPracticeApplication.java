df561346b8b4a8ceed1b125d474b96b005ec6511
sam-11209 <shubham11209@gmail.com>
2020-11-08 02:08:40 +0530

    :add port in application.properties :sysout in main :first commit

---
+++ src/main/java/com/git/GitHubPracticeApplication.java
@@ -0,0 +1,14 @@
+package com.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubPracticeApplication.class, args);
+		System.out.println("----git hub running-----------");
+	}
+
+}
