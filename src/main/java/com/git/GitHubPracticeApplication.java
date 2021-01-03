620c63ed741cbf5f50ae10c75d47546797f655ef
sam-11209 <shubham11209@gmail.com>
2021-01-04 02:39:32 +0530

    remove all sysout

--- src/main/java/com/git/GitHubPracticeApplication.java
+++ src/main/java/com/git/GitHubPracticeApplication.java
@@ -1,6 +1,6 @@
 package com.git;
 
-import org.springframework.boot.SpringApplication;
+ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -8,11 +8,7 @@ public class GitHubPracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitHubPracticeApplication.class, args);
-		System.out.println("----git hub running-----------");
-		System.out.println("----adding something from git hub -----------");
-		System.out.println("ABC");
-		System.out.println("ABCD");
-		System.out.println("----adding something from git hub again -----------");
+	
 	}
 	
 	public void gitUndrstand() {
