23a9eea0419328ce3d0d64840b42c0c749cba81b
sam-11209 <39234443+sam-11209@users.noreply.github.com>
2021-01-05 01:41:10 +0530

    added three github sysout

--- src/main/java/com/git/GitHubPracticeApplication.java
+++ src/main/java/com/git/GitHubPracticeApplication.java
@@ -8,6 +8,9 @@ public class GitHubPracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitHubPracticeApplication.class, args);
+		System.out.println("GITHUB1");
+		System.out.println("GITHUB2");
+		System.out.println("GITHUB3");
 	
 	}
 	
